8ab4c4025aafd6b8516a96e675c32cab38b15e30
alyrow <58048578+alyrow@users.noreply.github.com>
2022-07-13 18:49:12 +0200

    add test in group res

--- src/main/kotlin/fr/corpauration/group/GroupResource.kt
+++ src/main/kotlin/fr/corpauration/group/GroupResource.kt
@@ -1,14 +1,24 @@
 package fr.corpauration.group
 
+import fr.corpauration.utils.BaseEntity
 import fr.corpauration.utils.BaseRepository
 import fr.corpauration.utils.BaseResource
+import io.vertx.mutiny.pgclient.PgPool
+import javax.inject.Inject
 import javax.ws.rs.GET
 import javax.ws.rs.Path
 
 @Path("/group")
 class GroupResource : BaseResource() {
+
+    @Inject
+    lateinit var client: PgPool
     @GET
-    fun hello() = "Hello from /group"
+    fun hello(): String {
+        val repo: BaseRepository<Int, BaseEntity> = BaseRepository(client, "test")
+        repo.findAll()
+        return "Hello from /group"
+    }
 }
 
 @Path("/groups")
--- src/main/kotlin/fr/corpauration/utils/BaseRepository.kt
+++ src/main/kotlin/fr/corpauration/utils/BaseRepository.kt
@@ -4,29 +4,45 @@ import io.smallrye.mutiny.Multi
 import io.smallrye.mutiny.Uni
 import io.vertx.mutiny.pgclient.PgPool
 import io.vertx.mutiny.sqlclient.Row
+import io.vertx.mutiny.sqlclient.RowIterator
 import io.vertx.mutiny.sqlclient.RowSet
 import io.vertx.mutiny.sqlclient.Tuple
 import org.reactivestreams.Publisher
-import javax.inject.Inject
+import java.util.function.Function
+import javax.transaction.Transactional
 
-class BaseRepository<K, V: BaseEntity>(var table: String) {
-    @Inject
-    var client: PgPool? = null
+class BaseRepository<K, V: BaseEntity>(var client: PgPool, var table: String) {
 
     fun battle(r: Any): V {
         print(r)
         return object {} as V
     }
+    fun battle2(r: Any): V {
+        print(r)
+        print("efzefzef")
+        return object {} as V
+    }
+
+    @Transactional
     fun findAll(): List<V> {
-        val rowSet: Uni<RowSet<Row>> = client!!.preparedQuery("SELECT * FROM $1").execute(Tuple.of(table))
+        print("print")
+        val rowSet: Uni<RowSet<Row>> = client.preparedQuery("SELECT * FROM $1").execute(Tuple.of(table))
+        /*print(rowSet.onItem().transform(RowSet<Row>::iterator)
+            .onItem().tra)*/
         val whatsTheType = rowSet.onItem().transformToMulti(java.util.function.Function<RowSet<Row>, Publisher<*>> {
             set: RowSet<Row> -> Multi.createFrom().iterable(set)
         }).onItem().transform(java.util.function.Function<Any, V> {
                 row: Any -> battle(row)
         })
 
-        return TODO()
+
+        return emptyList()
     }
+
+    /*fun oskour(): Multi<Any> {
+        val rowSet: Uni<RowSet<Row>> = client.preparedQuery("SELECT * FROM $1").execute(Tuple.of(table))
+        return rowSet.onItem().transformToMulti(set -> Multi.createFrom().iterable(set)).onItem().transform(Fruit::from);
+    }*/
 
     fun findById(id: K): V {
         return TODO()
