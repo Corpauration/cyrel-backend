6b23242e9b7d0088cae3707e748a94dd04f9b724
alyrow <58048578+alyrow@users.noreply.github.com>
2022-07-16 20:25:29 +0200

    refactor RepositoryGeneratorProcessor.kt

--- processor/src/main/kotlin/fr/corpauration/utils/RepositoryGeneratorProcessor.kt
+++ processor/src/main/kotlin/fr/corpauration/utils/RepositoryGeneratorProcessor.kt
@@ -67,49 +67,7 @@ class RepositoryGeneratorProcessor(
         }
 
         override fun visitFunctionDeclaration(function: KSFunctionDeclaration, data: KSAnnotation?) {
-            val parent = function.parentDeclaration as KSClassDeclaration
-            val packageName = parent.containingFile!!.packageName.asString()
-            val className = parent.simpleName.asString()
-            val file = codeGenerator.createNewFile(Dependencies(true, function.containingFile!!), packageName , className)
-            file.appendText(ClassBuilder(packageName, className)
-                .addImport("io.vertx.mutiny.pgclient.PgPool")
-                .add { input: ClassBuilder -> generateGetAll(input) }
-                .build())
-            /*file.appendText("package $packageName\n\n")
-//            file.appendText("import HELLO\n\n")
-            file.appendText("class $className{\n")
-            function.parameters.forEach {
-                val name = it.name!!.asString()
-                val typeName = StringBuilder(it.type.resolve().declaration.qualifiedName?.asString() ?: "<ERROR>")
-                val typeArgs = it.type.element!!.typeArguments
-                if (it.type.element!!.typeArguments.isNotEmpty()) {
-                    typeName.append("<")
-                    typeName.append(
-                        typeArgs.map {
-                            val type = it.type?.resolve()
-                            "${it.variance.label} ${type?.declaration?.qualifiedName?.asString() ?: "ERROR"}" +
-                                    if (type?.nullability == Nullability.NULLABLE) "?" else ""
-                        }.joinToString(", ")
-                    )
-                    typeName.append(">")
-                }
-                file.appendText("    private var $name: $typeName? = null\n")
-                file.appendText("    internal fun with${name.capitalize()}($name: $typeName): $className {\n")
-                file.appendText("        this.$name = $name\n")
-                file.appendText("        return this\n")
-                file.appendText("    }\n\n")
-            }
-            file.appendText("    internal fun build(): ${parent.qualifiedName!!.asString()} {\n")
-            file.appendText("        return ${parent.qualifiedName!!.asString()}(")
-            file.appendText(
-                function.parameters.map {
-                    "${it.name!!.asString()}!!"
-                }.joinToString(", ")
-            )
-            file.appendText(")\n")
-            file.appendText("    }\n")
-            file.appendText("}\n")*/
-            file.close()
+
         }
 
         fun generateGetAll(builder: ClassBuilder): ClassBuilder {
