d5b9f011b08d553cf1145939361b1637768cf2f1
alyrow <58048578+alyrow@users.noreply.github.com>
2022-07-26 15:37:56 +0200

    `/ping` return Uni

--- src/main/kotlin/fr/corpauration/utils/BaseResource.kt
+++ src/main/kotlin/fr/corpauration/utils/BaseResource.kt
@@ -1,10 +1,11 @@
 package fr.corpauration.utils
 
+import io.smallrye.mutiny.Uni
 import javax.ws.rs.GET
 import javax.ws.rs.Path
 
 open class BaseResource {
     @Path("/ping")
     @GET
-    fun ping() = "PONG!"
+    fun ping() = Uni.createFrom().item("PONG!")
 }
