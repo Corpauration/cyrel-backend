54955c371a69a492efcd8a4e1490303dbe818e7e
alyrow <58048578+alyrow@users.noreply.github.com>
2022-08-17 14:18:01 +0200

    add some logging

--- src/main/kotlin/fr/corpauration/security/SecurityResource.kt
+++ src/main/kotlin/fr/corpauration/security/SecurityResource.kt
@@ -1,5 +1,6 @@
 package fr.corpauration.security
 
+import fr.corpauration.ExampleResource
 import io.ktor.client.*
 import io.ktor.client.call.*
 import io.ktor.client.engine.cio.*
@@ -8,11 +9,13 @@ import io.ktor.client.request.forms.*
 import io.ktor.client.statement.*
 import io.ktor.http.*
 import io.ktor.serialization.jackson.*
+import org.jboss.logging.Logger
 import javax.enterprise.context.ApplicationScoped
 import javax.ws.rs.POST
 import javax.ws.rs.Path
 import javax.ws.rs.Produces
 import javax.ws.rs.core.MediaType
+
 
 @Path("/security")
 @Produces(MediaType.APPLICATION_JSON)
@@ -24,19 +27,22 @@ class SecurityResource {
         }
     }
 
+    private val LOG: Logger = Logger.getLogger(SecurityResource::class.java)
+
     @POST
     suspend fun getToken(credentials: Credentials): TokenResponse {
         val response: HttpResponse = client.submitForm(
-            url = "http://localhost:6969/realms/test-realm/protocol/openid-connect/token",
+            url = "${System.getenv("KEYCLOAK_URL")}/protocol/openid-connect/token",
             formParameters = Parameters.build {
                 append("client_id", "cyrel")
                 append("grant_type", "password")
-                append("client_secret", System.getenv("CLIENT_SECRET"))
+                append("client_secret", System.getenv("KEYCLOAK_SECRET"))
                 append("scope", "openid")
                 append("username", credentials.username)
                 append("password", credentials.password)
             }
         )
+        if (!response.status.isSuccess()) LOG.error(response.request.url)
         return response.body()
     }
 }
