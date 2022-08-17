a6cc8b5dec277458ec9b400db3a0497972e3654e
alyrow <58048578+alyrow@users.noreply.github.com>
2022-08-17 14:18:57 +0200

    fix incorrect type

--- src/main/kotlin/fr/corpauration/security/TokenResponse.kt
+++ src/main/kotlin/fr/corpauration/security/TokenResponse.kt
@@ -2,4 +2,14 @@ package fr.corpauration.security
 
 import com.fasterxml.jackson.annotation.JsonAlias
 
-data class TokenResponse(@JsonAlias("access_token") val accessToken: String, @JsonAlias("refresh_token") val refreshToken: String, @JsonAlias("id_token") val idToken: String, @JsonAlias("expires_in") val expiresIn: Int, @JsonAlias("refresh_expires_in") val refreshExpiresIn: Int, @JsonAlias("token_type") val tokenType: String, @JsonAlias("not-before-policy") val notBeforePolicy: String, @JsonAlias("session_state") val sessionState: String, val scope: String)
+data class TokenResponse(
+    @JsonAlias("access_token") val accessToken: String,
+    @JsonAlias("refresh_token") val refreshToken: String,
+    @JsonAlias("id_token") val idToken: String,
+    @JsonAlias("expires_in") val expiresIn: Int,
+    @JsonAlias("refresh_expires_in") val refreshExpiresIn: Int,
+    @JsonAlias("token_type") val tokenType: String,
+    @JsonAlias("not-before-policy") val notBeforePolicy: Int,
+    @JsonAlias("session_state") val sessionState: String,
+    val scope: String
+)
