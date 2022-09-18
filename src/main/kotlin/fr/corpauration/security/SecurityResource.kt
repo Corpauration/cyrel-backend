73d75c310b9f895227153b8ece52973268af1eea
alyrow <58048578+alyrow@users.noreply.github.com>
2022-09-18 10:57:58 +0200

    add logout method

--- src/main/kotlin/fr/corpauration/security/SecurityResource.kt
+++ src/main/kotlin/fr/corpauration/security/SecurityResource.kt
@@ -10,10 +10,7 @@ import io.ktor.http.*
 import io.ktor.serialization.jackson.*
 import org.jboss.logging.Logger
 import javax.enterprise.context.ApplicationScoped
-import javax.ws.rs.POST
-import javax.ws.rs.PUT
-import javax.ws.rs.Path
-import javax.ws.rs.Produces
+import javax.ws.rs.*
 import javax.ws.rs.core.MediaType
 
 
@@ -60,5 +57,18 @@ class SecurityResource {
         )
         if (!response.status.isSuccess()) LOG.error(response.request.url)
         return response.body()
+    }
+
+    @DELETE
+    suspend fun logout(refreshToken: String) {
+        val response: HttpResponse = client.submitForm(
+            url = "${System.getenv("KEYCLOAK_URL")}/protocol/openid-connect/logout",
+            formParameters = Parameters.build {
+                append("client_id", "cyrel")
+                append("client_secret", System.getenv("KEYCLOAK_SECRET"))
+                append("refresh_token", refreshToken)
+            }
+        )
+        if (!response.status.isSuccess()) LOG.error(response.request.url)
     }
 }
