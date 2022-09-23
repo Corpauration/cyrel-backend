eeb97f72499429c5fdb9b2543dd82426aa926c15
alyrow <58048578+alyrow@users.noreply.github.com>
2022-09-23 08:25:37 +0200

    add default themes

---
+++ src/main/resources/db/migration/V14__Add_data_in_themes.sql
@@ -0,0 +1,7 @@
+INSERT INTO themes (id, background, foreground, card, "navIcon")
+VALUES (0, '4294440952', '4278190080', '4294967295', '4292993505')
+ON CONFLICT DO NOTHING;
+
+INSERT INTO themes (id, background, foreground, card, "navIcon")
+VALUES (1, '4279374353', '4294967295', '4278190080', '4280492835')
+ON CONFLICT DO NOTHING;
