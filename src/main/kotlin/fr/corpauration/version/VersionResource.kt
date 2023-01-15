9c9df5bbf36e66fe22fdb94b7e9f57f92d99aab7
alyrow <58048578+alyrow@users.noreply.github.com>
2023-01-15 17:57:24 +0100

    add endpoint for getting latest client version

--- src/main/kotlin/fr/corpauration/version/VersionResource.kt
+++ src/main/kotlin/fr/corpauration/version/VersionResource.kt
@@ -1,10 +1,14 @@
 package fr.corpauration.version
 
+import fr.corpauration.utils.RepositoryGenerator
 import io.smallrye.mutiny.Uni
 import org.eclipse.microprofile.config.inject.ConfigProperty
+import org.jboss.resteasy.reactive.RestQuery
 import org.jboss.resteasy.reactive.RestResponse
 import org.jboss.resteasy.reactive.server.ServerExceptionMapper
+import java.util.*
 import javax.enterprise.context.ApplicationScoped
+import javax.inject.Inject
 import javax.ws.rs.GET
 import javax.ws.rs.POST
 import javax.ws.rs.Path
@@ -18,6 +22,14 @@ import javax.ws.rs.core.Response
 class VersionResource {
     @ConfigProperty(name = "quarkus.application.version")
     lateinit var version: String
+
+    @Inject
+    @RepositoryGenerator(
+        table = "version",
+        id = Boolean::class,
+        entity = VersionEntity::class
+    )
+    lateinit var versionRepository: VersionRepository
 
     @ServerExceptionMapper
     fun mapException(x: UnknownCanal): RestResponse<String>? {
@@ -34,5 +46,11 @@ class VersionResource {
         val v = CyrelVersion.fromString(version)
         val b = CyrelVersion.fromString(clientVersion)
         return Uni.createFrom().item(b.isAtLeast(v.major, v.minor))
+    }
+
+    @GET
+    @Path("/client/{platform}")
+    fun getClientLastVersion(platform: String): Uni<VersionEntity> {
+        return versionRepository.findBy(platform, "platform").collect().first()
     }
 }
