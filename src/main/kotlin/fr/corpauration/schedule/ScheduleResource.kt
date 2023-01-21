c7d56cc21197ea88f2614a9759a41598a31b0d55
alyrow <58048578+alyrow@users.noreply.github.com>
2023-01-21 15:18:31 +0100

    add endpoints for getting professors schedule

--- src/main/kotlin/fr/corpauration/schedule/ScheduleResource.kt
+++ src/main/kotlin/fr/corpauration/schedule/ScheduleResource.kt
@@ -1,12 +1,12 @@
 package fr.corpauration.schedule
 
 import com.fasterxml.jackson.databind.JsonNode
+import fr.corpauration.group.ADMIN
 import fr.corpauration.group.GroupRepository
-import fr.corpauration.homework.UnauthorizedGroupTarget
 import fr.corpauration.user.UserRepository
-import fr.corpauration.user.UserType
 import fr.corpauration.utils.AccountExist
 import fr.corpauration.utils.CustomSql
+import fr.corpauration.utils.NeedToBeProfessorOrInGroups
 import fr.corpauration.utils.RepositoryGenerator
 import io.quarkus.security.Authenticated
 import io.quarkus.security.identity.SecurityIdentity
@@ -15,10 +15,7 @@ import io.smallrye.mutiny.Uni
 import java.time.LocalDateTime
 import javax.enterprise.context.ApplicationScoped
 import javax.inject.Inject
-import javax.ws.rs.BadRequestException
-import javax.ws.rs.POST
-import javax.ws.rs.Path
-import javax.ws.rs.Produces
+import javax.ws.rs.*
 import javax.ws.rs.core.MediaType
 
 @Path("/schedule")
@@ -54,14 +51,34 @@ class ScheduleResource {
         val start = LocalDateTime.parse(json.get("start").asText())
         val end = LocalDateTime.parse(json.get("end").asText())
 
-        return userRepository.findBy(identity.principal.name, "email").collect().asList().onItem()
-            .transform { it[0] }
-            .flatMap {
-                if (it.groups.map { it.id }
-                        .contains(json.get("group").asInt()) || it.type == UserType.PROFESSOR.ordinal)
-                    wrapperRetrieveScheduleForGroupBetweenDate(json.get("group").asInt(), start, end).collect().asList()
-                else throw UnauthorizedGroupTarget()
-            }
+        return wrapperRetrieveScheduleForGroupBetweenDate(json.get("group").asInt(), start, end).collect().asList()
+    }
+
+    @GET
+    @Path("/professors")
+    @NeedToBeProfessorOrInGroups(ADMIN)
+    @Produces(MediaType.APPLICATION_JSON)
+    fun getScheduleProfessors(): Uni<List<String>> {
+        return wrapperRetrieveScheduleProfessors().collect().asList()
+    }
+
+    @POST
+    @Path("/professors")
+    @NeedToBeProfessorOrInGroups(ADMIN)
+    @Produces(MediaType.APPLICATION_JSON)
+    fun getProfessorScheduleFromTo(json: JsonNode): Uni<List<CourseEntity>> {
+        if (!json.hasNonNull("professor") || !json.get("professor").isTextual || !json.hasNonNull(
+                "start"
+            ) || !json.get("start").isTextual || !json.hasNonNull("end") || !json.get("end").isTextual
+        ) throw BadRequestException()
+        val start = LocalDateTime.parse(json.get("start").asText())
+        val end = LocalDateTime.parse(json.get("end").asText())
+
+        return wrapperRetrieveScheduleProfessors().collect().asList().flatMap {
+            if (it.contains(json.get("professor").asText()))
+                wrapperRetrieveScheduleForProfessorBetweenDate(json.get("professor").asText(), start, end).collect().asList()
+            else throw Exception()
+        }
     }
 
     @CustomSql(
@@ -77,5 +94,28 @@ class ScheduleResource {
         end: LocalDateTime
     ): Multi<CourseEntity> {
         return courseRepository.wrapperRetrieveScheduleForGroupBetweenDate(group, start, end)
+    }
+
+    @CustomSql(
+        """
+        select distinct regexp_split_to_table(teachers, ',') from courses where teachers <> '' ORDER BY regexp_split_to_table(teachers, ',') asc 
+    """, entity = CourseEntity::class
+    )
+    fun wrapperRetrieveScheduleProfessors(): Multi<String> {
+        return courseRepository.wrapperRetrieveScheduleProfessors()
+    }
+
+    @CustomSql(
+        """
+        select c.* from courses c, courses_groups cg
+        where c.id = cg.id and c.teachers ilike '%'||$1||'%' and c.start >= $2 and c."end" <= $3
+    """, entity = CourseEntity::class
+    )
+    fun wrapperRetrieveScheduleForProfessorBetweenDate(
+        professor: String,
+        start: LocalDateTime,
+        end: LocalDateTime
+    ): Multi<CourseEntity> {
+        return courseRepository.wrapperRetrieveScheduleForProfessorBetweenDate(professor, start, end)
     }
 }
